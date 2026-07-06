84aacb7ee4627f8690464cb9e223b140477ef5cd
Zac Downie <14842@holbertonstudents.com>
2026-07-06 15:50:34 +1000

    updated

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
-/*
- *
+/**
  * main - Prints using print f
  *
  * Return 0 always
- *
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
