e1c7d56fb751078d9607ed0283165233fc06bd77
Zac Downie <14842@holbertonstudents.com>
2026-07-06 05:33:00 +0000

    writing c code

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,14 @@
-#!/bin/bash
-put "Programming is like building a multilingual puzzle"
+#include <studio.sh>
+
+/**
+ *
+ * main - Prints a string using puts
+ *
+ * Return: 0 always
+ *
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
