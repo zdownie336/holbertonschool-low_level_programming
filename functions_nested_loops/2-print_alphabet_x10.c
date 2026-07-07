99da463b87490dc1bc5f97a8fa26e7e46e56e711
Zac Downie <14842@holbertonstudents.com>
2026-07-07 18:49:02 +1000

    tweaked code

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -8,12 +8,12 @@
 int print_alphabet_x10(void)
 {
 int i;
-int count;
-	while (count = 10; count > 0)
+int count = 10;
+	while (count > 0)
 	{
 	for (i = 97; i <= 122; i++)
 	{
-		_putchar(1);
+		_putchar(i);
 	}
 count--;
 	}
