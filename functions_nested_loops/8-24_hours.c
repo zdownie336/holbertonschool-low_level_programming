98d54e7666dbe4c748d5df01ea9b6fcbc67ff053
Zac Downie <14842@holbertonstudents.com>
2026-07-07 20:45:02 +1000

    time printer program

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer - all minutes and hours of the day
+ *
+ * Return: always 0
+ */
+void jack_bauer(void)
+{
+	int hour;
+	int min;
+
+	for (hour = 0; hour <= 23; hour++)
+	{
+		for (min = 0; min <= 59; min++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			-putchar((min % 10 + '0');
+			_putchar('\n');
+			}
+			}
+			}
