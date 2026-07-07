eadc1ef3ed83ec2b0aebce7fd5d91e14bdf904f8
Zac Downie <14842@holbertonstudents.com>
2026-07-07 15:13:10 +1000

    prints 0-9

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print all single digits of base 10 from 0
+ *
+ * Return: 0 Always
+ */
+int main(void)
+{
+int n;
+
+for (n = 48; <= 57; n++)
+{
+putchar(n);
+}
+putchar('\n')
+return (0);
+}
