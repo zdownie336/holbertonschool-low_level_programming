9235566530fbf13badd31a66deae9cb3c9e9148e
Zac Downie <14842@holbertonstudents.com>
2026-07-07 21:26:46 +1000

    createn adition program

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - displays results of two integers combined
+ *@n1: first integer
+ *@n2: second integer
+ *
+ * Return: Always "result"
+ */
+int add(int n1, int n2)
+{
+return (n1 + n2);
+}
