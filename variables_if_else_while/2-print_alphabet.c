e88a17894e00e927ab9d7cb51609a12e48751df4
Zac Downie <14842@holbertonstudents.com>
2026-07-07 14:22:54 +1000

    created file

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 /**
- * main - printing alphabet in lower case alphabet
- *
- * return: 0 always
- */
+* main - printing alphabet in lower case alphabet
+*
+* return: 0 always
+*/
 
 int main(void)
 {
-	int i;
-	
-	for (i=97; i <=127; i++)
-	{
+int i;
+
+for (i = 97; i <= 127; i++)
+{
 putchar(i);
-	}
-	putchar('\n');
-		return (0);
+}
+putchar('\n');
+return (0);
 }
