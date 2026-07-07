5406ae3391140eff9deecadacf717232a9dd941f
Zac Downie <14842@holbertonstudents.com>
2026-07-07 20:47:48 +1000

    changed '-' to '_'

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -7,19 +7,19 @@
  */
 void jack_bauer(void)
 {
-	int hour;
-	int min;
+int hour;
+int min;
 
-	for (hour = 0; hour <= 23; hour++)
-	{
-		for (min = 0; min <= 59; min++)
-		{
-			_putchar((hour / 10) + '0');
-			_putchar((hour % 10) + '0');
-			_putchar(':');
-			_putchar((min / 10) + '0');
-			-putchar((min % 10 + '0');
-			_putchar('\n');
-			}
-			}
-			}
+for (hour = 0; hour <= 23; hour++)
+{
+for (min = 0; min <= 59; min++)
+{
+_putchar((hour / 10) + '0');
+_putchar((hour % 10) + '0');
+_putchar(':');
+_putchar((min / 10) + '0');
+_putchar((min % 10 + '0');
+_putchar('\n');
+}
+}
+}
