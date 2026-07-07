aa90c1e2318a16c9bd093aaf643a8002c6affbaf
Zac Downie <14842@holbertonstudents.com>
2026-07-07 13:51:02 +1000

    updated files

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - generating number and displaying last digit if higher than 6 or is 0
+ *
+ * Return: 0 always
+ */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+last_digit = n % 10;
+printf("last_digit of %d is %d \n", n, last_digit);
+	if (last_digit == 0)
+{
+	printf("and is 0\n");
+}
+else if (last_digit > 5)
+{
+	printf("and is greater than 5\n");
+}
+else
+{
+	printf("and is less than 6 and not 0\n");
+}
+	return (0);
+}
--- variables_if_else_while/README.md
+++ variables_if_else_while/README.md
@@ -1,1 +1,3 @@
 Read Me explaining the scripts and functions within the directory
+file 0 refers to a script to generate a random number and tell the user if it is positive or negaive, this number is different per execution
+file 1 is similar to file 0, however displays only the last digit of the number and displays if its 0, less than 6 and not 0 or if it's more than 5
