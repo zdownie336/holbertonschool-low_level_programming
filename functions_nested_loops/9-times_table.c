50609ffc75149c86181ead59c640970b1fe08a16
Zac Downie <14842@holbertonstudents.com>
2026-07-07 21:11:09 +1000

    tweaked table character amount

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
-* times_table - creating a times table for 9
+* times_table - print times table in a table format
 *
-* Return: Always void
+* Return: always void
 */
 void times_table(void)
 {
@@ -15,22 +15,22 @@ for (i = 0; i <= 9; i++)
 {
 _putchar('0');
 
-for (j = 0; j <= 9; j++)
+for (j = 1; j <= 9; j++)
 {
 table = i * j;
 _putchar(',');
 _putchar(' ');
 if (table < 10)
 {
-	_putchar(' ');
-	_putchar(table + '0');
+_putchar(' ');
+_putchar(table + '0');
 }
 else
 {
-	_putchar(table / 10 + '0');
-	_putchar(table % 10 + '0');
+_putchar(table / 10 + '0');
+_putchar(table % 10 + '0');
 }
 }
 _putchar('\n');
 }
-}
+}i
