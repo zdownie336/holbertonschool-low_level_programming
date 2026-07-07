60462d6060f6a8ce1cd087933dfcef16a9cc478d
Zac Downie <14842@holbertonstudents.com>
2026-07-07 15:28:02 +1000

    reverse alphabet program

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet in reverse
+ *
+ * Return: 0 Always
+ */
+int main(void)
+{
+	int z;
+
+	for (z = 122; z >= 97; z--)
+	{
+		putchar(z);
+	}
+	putchar('\n');
+		return (0);
+}
