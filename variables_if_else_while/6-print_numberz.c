887ef3597ef88076b91b78d82c4dd83f06f28353
Zac Downie <14842@holbertonstudents.com>
2026-07-07 15:23:38 +1000

    created 0-9 file

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Print 0-9 only ussing putchar command
+ *
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+	putchar(n);
+	}
+	putchar('\n');
+		return (0);
+}
