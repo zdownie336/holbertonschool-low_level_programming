fcc970f8f0f032496cc6b2629007308a3139cba7
Zac Downie <14842@holbertonstudents.com>
2026-07-07 13:54:48 +1000

    removed n

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -16,7 +16,7 @@ int last_digit;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 last_digit = n % 10;
-printf("last_digit of %d is %d \n", n, last_digit);
+printf("last_digit of %d is %d", n, last_digit);
 	if (last_digit == 0)
 {
 	printf("and is 0\n");
