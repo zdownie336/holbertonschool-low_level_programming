f70918865c7decda5363dda0957ce1d176cfb2b1
Zac Downie <14842@holbertonstudents.com>
2026-07-07 14:12:08 +1000

    created abc... script

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - printing alphabet in lower case alphabet
+ *
+ * return: 0 always
+ */
+
+int main(void)
+{
+	int i;
+	
+	for (i=97; i <=127; i++)
+	{
+putchar(i);
+	}
+	putchar('\n');
+		return (0);
+}
