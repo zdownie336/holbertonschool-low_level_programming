3228879e8e21fddb3ef099d86fe6315067088fdc
Zac Downie <14842@holbertonstudents.com>
2026-07-07 20:22:32 +1000

    absolute integer value program

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _abs - prints the absolute value of an integer
+*@n: number being conv erted
+*
+* Return: Always 0.
+*/
+int _abs(int n)
+{
+if (n >= 0)
+{
+return (n);
+}
+return (n * -1);
+}
