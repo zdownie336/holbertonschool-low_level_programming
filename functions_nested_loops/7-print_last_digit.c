a145cea9598a3d98ff2eacacb4cb0d67ce897692
Zac Downie <14842@holbertonstudents.com>
2026-07-07 20:36:58 +1000

    forgot a ; for int last

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,15 @@
 */
 int print_last_digit(int n)
 {
-int last
-	last = n % 10;
+int last;
+last = n % 10;
+
 if (last < 0)
 {
-	last *= -1;
+last *= -1;
 }
+
 _putchar(last +'0');
+
 return (last);
 }
