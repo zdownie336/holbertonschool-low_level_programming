5bbecfbe0d3be0c1d5f46566943925ae11fa0b0a
Zac Downie <14842@holbertonstudents.com>
2026-07-07 14:53:26 +1000

    capitalisation and betty approved

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints both capitalised and lower case alphabets
+ *
+ * Return: 0 Always (Success)
+*/
+
+int main(void)
+{
+	int i;
+	int a;
+
+	for (i = 97; i <= 122; i++)
+	{
+	putchar(i);
+	}
+	for (a = 65; i <= 90; a++)
+	{
+	putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
