cba8ea905aabac4159555a71f53844146e474f9b
Zac Downie <14842@holbertonstudents.com>
2026-07-07 18:24:08 +1000

    created alpahbet program

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	print_alphabet();
+	return (0);
+}
