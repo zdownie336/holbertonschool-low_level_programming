b49230fdedb7e8acaa150bfae3bfb9a0fc80ced4
Zac Downie <14842@holbertonstudents.com>
2026-07-07 18:54:36 +1000

    decomplicated it

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -8,15 +8,14 @@
 int print_alphabet_x10(void)
 {
 int i;
-int count = 10;
-	while (count > 0)
+int r;
+	for (r = 0; r <= 9; r++) 
 	{
 	for (i = 97; i <= 122; i++)
 	{
 		_putchar(i);
-		_putchar('\n');
 	}
-count--;
+_putchar('\n');
 	}
 	return (0);
 }
