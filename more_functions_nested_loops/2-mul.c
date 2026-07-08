dcdae14824052288fa7d1a4f815af13171ce4b33
Zac Downie <14842@holbertonstudents.com>
2026-07-09 08:38:10 +1000

    multiplication program

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* mul - produce the result of multiplying two integars
+*@a: value for integar one
+*@b: value for integar two
+*
+* Return: result of a * b
+*/
+
+int mul(int a, int b)
+{
+return (a * b);
+}
