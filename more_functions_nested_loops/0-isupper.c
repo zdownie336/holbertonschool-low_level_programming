61fb0574829ff32ee3f3e4dba113c966f3436c38
Zac Downie <14842@holbertonstudents.com>
2026-07-08 11:35:10 +1000

    0-isupper.c

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,19 +1,20 @@
 #include "main.h"
 
 /**
- * _isupper - checks if entered character is uppercase returns 1 if true
- *@c: value being checked
- *
- * Return: Always 1 if uppercase, 0 if other character
- */
+* _isupper - checks if entered character is uppercase returns 1 if true
+*@c: value being checked
+*
+* Return: 1 if uppercase, 0 if other character
+*/
 int _isupper(int c)
 {
 if (c >= 65 && c <= 90)
 {
-	return (1);
+return (1);
 }
 else
 {
 return (0);
 }
+_putchar('\n');
 }
