e9bf105e09e2d9399c41eb69fc435dc183565c90
Zac Downie <14842@holbertonstudents.com>
2026-07-14 18:24:12 +1000

    fixed code

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -6,15 +6,15 @@
 */
 void print_rev(char *s)
 {
-int i;
-	i = 0;
+	int i;
 
+	i = 0;
 	while (s[i] != '\0')
 	{
 		i++;
 	}
 	i--;
-	while (s[i] >= 0)
+	while (i >= 0)
 	{
 		_putchar(s[i]);
 		i--;
