f46bd9cb2a1601c1664d5932032d7b70f53302e8
Zac Downie <14842@holbertonstudents.com>
2026-07-14 17:51:33 +1000

    took an hour to do

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*print_rev - printing the string in reverse
+*@s: reverse string
+*/
+void print_rev(char *s)
+{
+int i;
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	while (s[i] >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
