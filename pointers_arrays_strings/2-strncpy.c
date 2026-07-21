4e510032f1d8f4a4c82e2c8f2d62135a477ef9f0
Zac Downie <14842@holbertonstudents.com>
2026-07-21 23:05:41 +1000

    tweaked code

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -10,23 +10,18 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+    int i;
 
-	i = 0;
-
-	while (i < n && *src != '\0')
-	{
-		*dest = *src;
-		i++;
-		dest++;
-		src++;
-	}
-
-	while (i < n)
-	{
-		*dest = '\0';
-		i++;
-		dest++;
-	}
-	return (dest);
+    i = 0;
+    while (i < n && src[i] != '\0')
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    while (i < n)
+    {
+        dest[i] = '\0';
+        i++;
+    }
+    return (dest);
 }
