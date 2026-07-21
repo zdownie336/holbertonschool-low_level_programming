25ed7372c089363f96a750a4790d2c24b5d9513c
Zac Downie <14842@holbertonstudents.com>
2026-07-21 14:56:00 +1000

    tweaked code

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -11,20 +11,18 @@
 char *_strcat(char *dest, char *src)
 {
 	int i;
-	int j;
 
 	i = 0;
-	j = 0;
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
 
-	while (src[j] != '\0')
+	while (*src != '\0')
 	{
-		dest[i] = src[j];
+		dest[i] = *src;
 		i++;
-		j++;
+		src++;
 	}
 	dest[i] = '\0';
 
