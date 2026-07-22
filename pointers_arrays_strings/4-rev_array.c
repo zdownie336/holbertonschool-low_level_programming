f54a35914ca4a027d21947416484b95e021c3426
Zac Downie <14842@holbertonstudents.com>
2026-07-22 10:51:15 +1000

    tweaked code

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -9,11 +9,18 @@
 
 void reverse_array(int *a, int n)
 {
-	for (int i = 0, j = n - 1; i < j; i++, j--)
+	int i;
+	int j;
+
+	i = 0;
+	j = n - 1;
+	while (i < j)
 	{
 		int tmp = a[i];
 
 		a[i] = a[j];
 		a[j] = tmp;
+		i++;
+		j--;
 	}
 }
