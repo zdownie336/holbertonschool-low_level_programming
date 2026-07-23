d556b9bf35ed4c6e57d65c8de095547607f2176a
Zac Downie <14842@holbertonstudents.com>
2026-07-23 20:07:01 +1000

    fixed betty

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -10,9 +10,9 @@
  */
 struct dog
 {
- char *name;
- float age;
- char *owner;
+char *name;
+float age;
+char *owner;
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
@@ -20,5 +20,5 @@ void print_dog(struct dog *d);
 *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endif // !DOG_H
+#endif
 
