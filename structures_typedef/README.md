9ed148eb04a6db9d200496f31d5b375c50fb4fce
Zac Downie <14842@holbertonstudents.com>
2026-07-23 20:06:04 +1000

    files created

---
+++ structures_typedef/README.md
@@ -0,0 +1,3 @@
+### This file contains the header and prototypes for the struct of dog.h
+
+This file follows the projects to make and understand structs and how typedef works,
---
+++ structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - information for the dog
+ * @name: dog's name
+ * @age: age of the dog
+ * @owner: dog owner's name
+ *
+ * Description: the dog has a name, owner and age.
+ */
+struct dog
+{
+ char *name;
+ float age;
+ char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+*new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif // !DOG_H
+
