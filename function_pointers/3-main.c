b17e736a71022ca29c5b477d84a036ba9e419501
Zac Downie <14842@holbertonstudents.com>
2026-07-29 10:48:52 +1000

    changed error codes

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
 	if (argc != 4)
 	{
 		printf("Error\n");
-		exit(1);
+		exit(98);
 	}
 
 	op = get_op_func(argv[2]);
@@ -27,14 +27,14 @@ int main(int argc, char *argv[])
 	if (op == NULL)
 	{
 		printf("Error\n");
-		exit(2);
+		exit(99);
 	}
 
 	if ((strcmp(argv[2], "/") == 0 || strcmp(argv[2], "%") == 0)
 		&& strcmp(argv[3], "0") == 0)
 	{
 		printf("Error\n");
-		exit(-1);
+		exit(100);
 	}
 
 	result = op(atoi(argv[1]), atoi(argv[3]));
