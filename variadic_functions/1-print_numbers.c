6a3ddfe18138b495a50a1d974f5890ea6da2e1dc
Zac Downie <14842@holbertonstudents.com>
2026-08-03 10:17:06 +1000

    made file

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,47 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+* print_numbers - printing the array of integars being designated
+* @n: input value
+* @separator: value for seperating ints
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list numbers;
+	unsigned int i;
+	int j;
+
+	i = 0;
+
+	if (separator == NULL)
+	{
+		separator = "";
+	}
+
+	if (n == 0)
+	{
+		printf("\n");
+		return;
+	}
+
+	va_start(numbers, n);
+
+	printf("%d", va_arg(numbers, int));
+
+	while (i < n - 1)
+	{
+		j = 0;
+		while (separator[j] == '\0')
+		{
+			printf("%c", separator[j]);
+			j = j + 1;
+		}
+		printf("%d\n", va_arg(numbers, int));
+		i = i + 1;
+	}
+	printf("\n");
+
+	va_end(numbers);
+}
