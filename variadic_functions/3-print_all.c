8523a712cf64e1e6465797492dcc4c7e14e0c3ba
Zac Downie <14842@holbertonstudents.com>
2026-08-03 11:16:02 +1000

    updated

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -13,32 +13,39 @@ void print_all(const char * const format, ...)
 	va_list arg;
 	unsigned int i;
 	char *str;
+	unsigned int j;
 
 	va_start(arg, format);
 
 	i = 0;
 	while (format != NULL && format[i] != '\0')
 	{
+		j = 0;
 		switch (format[i])
 		{
 			case 'c':
 				printf("%c, ", va_arg(arg, int));
+				j = 1;
 				break;
 			case 'i':
 				printf("%d, ", va_arg(arg, int));
+				j = 1;
 				break;
 			case 'f':
 				printf("%f, ", va_arg(arg, double));
+				j = 1;
 				break;
 			case 's':
 				str = va_arg(arg, char *);
 				if (str == NULL)
 				str = "(nil)";
-
-				 printf("%s, ", str);
+				printf("%s, ", str);
+				j = 1;
 				break;
 		}
 		i = i + 1;
+		if (format[i] && j == 1)
+			printf(", ");
 	}
 	printf("\n");
 	va_end(arg);
