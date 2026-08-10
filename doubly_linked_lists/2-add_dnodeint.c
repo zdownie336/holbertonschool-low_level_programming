d46415fe5a759386cebb100c46d51660ef5fe268
Zac Downie <14842@holbertonstudents.com>
2026-08-11 09:25:45 +1000

    swapped around *head and new_newnode in line 12

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,29 +1,37 @@
 #include "lists.h"
 
 /**
-* add_dnodeint - adding a new node to the beginning
-* @head: head node
-* @n: the integar data.
-*
-* Return: new doubly linked list.
-*/
-
+ * add_dnodeint - adding a node to the list
+ * @head: head of list
+ * @n: element to add
+ *
+ * Return: address of the new element NULL if failed
+ */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node;
 
 	if (head == NULL)
+	{
 		return (NULL);
+	}
 
-	new_node = malloc(sizeof(struct dlistint_s));
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
 
-		new_node->n = n;
-		new_node->next = *head;
-		new_node->prev = NULL;
+	new_node->n = n;
+	new_node->next = *head;
+	new_node->prev = NULL;
 
 	if (*head != NULL)
-		new_node->prev = *head;
+	{
+		(*head)->prev = new_node;
+	}
 
 	*head = new_node;
+
 	return (new_node);
 }
