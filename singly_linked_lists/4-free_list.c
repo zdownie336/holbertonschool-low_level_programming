3fee4e74dfea02bc4017c51e4a8cdc65c65cf9f0
Zac Downie <14842@holbertonstudents.com>
2026-08-11 16:18:36 +1000

    added description

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,13 +1,17 @@
 #include "lists.h"
 #include <stdlib.h>
 
+/**
+* free_list- freeing all linked lists.
+* @head: the header node.
+*/
 void free_list(list_t *head)
 {
 	list_t *temp;
 
 	while (head != NULL)
 	{
-		temp = head ->next;
+		temp = head->next;
 		free(head->str);
 		free(head);
 		head = temp;
