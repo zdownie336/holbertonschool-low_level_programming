d207a39d33594f15a6e1c4b46d79d5487ef3ea73
Zac Downie <14842@holbertonstudents.com>
2026-08-18 14:43:19 +1000

    betty compliance

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,5 +1,10 @@
 #include "main.h"
-
+/**
+* _strlen_recursion - counting the length of a string.
+* @s: the provided string.
+*
+* Return: length of string
+*/
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
