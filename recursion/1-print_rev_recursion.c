e6ae4da69006b696bb86ed36fd25407e91a888eb
Zac Downie <14842@holbertonstudents.com>
2026-08-18 10:36:09 +1000

    betty description

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,12 +1,15 @@
 #include "main.h"
-
+/**
+* _print_rev_recursion - reversing string then printing.
+* @s: the string.
+*/
 void _print_rev_recursion(char *s)
 {
 	int i = 0;
 
 	if (*s == '\0')
 		return;
-	
+
 	_print_rev_recursion(&s[i + 1]);
 	_putchar(s[i]);
 
